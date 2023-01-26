c8bf690c84be35a5342d05616983e5bbce7d4609
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-01-26 06:47:32 -0500

    Update rectcut.d

--- src/rectcut.d
+++ src/rectcut.d
@@ -1,7 +1,15 @@
 module rectcut;
+import std.algorithm;
 import std.traits;
 
 struct RectCut(T) if (isNumeric!(T))
 {
+    T minX, minY, maxX, maxY;
     
+    auto cutLeft(T a)
+    {
+        T originalX = minX;
+        minX = min(maxX, minX + a);
+        return RectCut(originalX, minY, minX, maxY);
+    }
 }
